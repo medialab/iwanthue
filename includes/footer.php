7bbc7afcf893d0243a45b48c56a0bafdecff32c6
Mathieu Jacomy <Mathieu.Jacomy@gmail.com>
2013-01-15 19:10:16 +0100

    Footer

--- includes/footer.php
+++ includes/footer.php
@@ -7,15 +7,27 @@
                     <div class="span4">
                        <!--  <a href="https://twitter.com/share" class="twitter-share-button" data-url="http://jiminy.medialab.sciences-po.fr/tools/palettes/" data-text="I Want Hue! Colors for Data Scientists" data-hashtags="dataviz">Tweet</a>
     <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-                         -->
                         <br/>
                         <br/>
                         
-                        [WORK IN PROGRESS]
+                         -->
+                        
+                        <p>
+                            We used:
+                            <br/>
+                            <strong><a href="#">Sigma.js</a></strong>,
+                            <a href="#">Prettify</a>,
+                            <a href="#">FileSaver.js</a>,
+                            <a href="#">Bootstrap</a>,
+                            <a href="#">jQuery</a>,
+                            <a href="#">Modernizr</a>,
+                            <a href="#">Initializr</a>
+                        </p>
                     
                     </div>
                     <div class="span4">
-                        <p>Überthanx to:</p>
+                        <p>See also our other tools at <strong><a href="http://tools.medialab.sciences-po.fr">Médialab Tools</a></strong>!</p>
+                        <p>And a huge <strong>thanks</strong> to these inspiring works:</p>
                         <strong><a href="https://github.com/gka/chroma.js">Chroma.js</a></strong>
                         <p>
                             I massively use this excellent js library to convert colors.
@@ -29,10 +41,11 @@
                     <div class="span4">
                         <div style="text-align:right">
                             <a href="http://medialab.sciences-po.fr"><img src="res/logosp_fonce.png"/></a>
-                            <br/>
-                            Developped by Mathieu Jacomy
-                            <br/>
-                            at the <a href="http://medialab.sciences-po.fr">Sciences-Po Medialab</a>
+                            <p class="muted">
+                                Developed by Mathieu Jacomy
+                                <br/>
+                                at the <a href="http://medialab.sciences-po.fr">Sciences-Po Medialab</a>
+                            </p>
                         </div>
                     </div>
                 </div>
