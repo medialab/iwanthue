eff9ff4b20d98549b6b32a9447a24519513d25db
Yomguithereal <guillaumeplique@gmail.com>
2020-12-02 10:42:15 +0100

    Chiller type declarations

--- npm/index.d.ts
+++ npm/index.d.ts
@@ -4,23 +4,23 @@ type ColorFilterFunction = (rgb: Color, lab: Color) => boolean;
 
 type ColorSpaceArray = [number, number, number, number, number, number];
 type ColorSpaceObject = {
-  hmin: number,
-  hmax: number,
-  cmin: number,
-  cmax: number,
-  lmin: number,
-  lmax: number
+  hmin?: number,
+  hmax?: number,
+  cmin?: number,
+  cmax?: number,
+  lmin?: number,
+  lmax?: number
 };
 type ColorSpace = string | ColorSpaceArray | ColorSpaceObject;
 
 type IWantHueOptions = {
-  colorFilter: ColorFilterFunction,
-  colorSpace: ColorSpace,
-  clustering: 'k-means' | 'force-vector',
-  quality: number,
-  ultraPrecision: boolean,
-  distance: 'euclidean' | 'cmc' | 'compromise' | 'protanope' | 'deuteranope' | 'tritanope',
-  seed: string | null
+  colorFilter?: ColorFilterFunction,
+  colorSpace?: ColorSpace,
+  clustering?: 'k-means' | 'force-vector',
+  quality?: number,
+  ultraPrecision?: boolean,
+  distance?: 'euclidean' | 'cmc' | 'compromise' | 'protanope' | 'deuteranope' | 'tritanope',
+  seed?: string | null
 };
 
 export default function iwanthue(colors: number, options?: IWantHueOptions): Array<string>;
