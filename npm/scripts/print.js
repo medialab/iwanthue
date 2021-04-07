b20d37482bfc8f4c59b18c5092a03c26da20a9a2
Yomguithereal <guillaumeplique@gmail.com>
2021-04-07 19:38:18 +0200

    Seeded experiment

--- npm/scripts/print.js
+++ npm/scripts/print.js
@@ -3,10 +3,16 @@ var chalk = require('chalk');
 var repeat = require('lodash/repeat')
 
 var n = +process.argv[2];
+var seed = process.argv[3]; // Examples: carefôjeffff, carefôjefffffg
 
-var palette = iwanthue(n, {
-  clustering: 'k-means'
-});
+var options = {
+  clustering: 'force-vector'
+};
+
+if (seed)
+  options.seed = seed;
+
+var palette = iwanthue(n, options);
 
 console.log();
 console.log('Generating palette with ' + n + ' colors.');
