e9cc5296cb4390002101b33f99c1b7829fe1b9ca
Paul Girard <paul@ouestware.com>
2026-02-19 16:53:44 +0100

    [npm] some more types

---
+++ npm/helpers.d.ts
@@ -0,0 +1,4 @@
+export function rgbHexToLab(hex: string): [number, number, number];
+export function labToHcl(
+  hcl: [number, number, number],
+): [number, number, number];
---
+++ npm/presets.d.ts
@@ -0,0 +1,4 @@
+import { ColorSpaceArray } from ".";
+
+declare const presets: Record<ColorSpacePreset, ColorSpaceArray>;
+export default presets;
